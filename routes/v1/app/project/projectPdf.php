b8943ca1d109a694270e5cb4ee23d929bfd9471b
sayednaweedsayedy <sayednaweedsayedy@gmail.com>
2025-09-09 22:08:16 +0430

    new download route

--- routes/v1/app/project/projectPdf.php
+++ routes/v1/app/project/projectPdf.php
@@ -6,5 +6,5 @@ use App\Enums\Permissions\PermissionEnum;
 use App\Http\Controllers\v1\app\projects\ProjectPdfController;
 
 Route::prefix('v1')->middleware(["authorized:" . 'organization:api'])->group(function () {
-  Route::get('projects/unsigned/mou/{id}', [ProjectPdfController::class, 'generateMou'])->middleware(["userHasMainPermission:" . PermissionEnum::projects->value . ',' . 'view']);
+  Route::get('projects/unsigned/mou', [ProjectPdfController::class, 'generateMou'])->middleware(["userHasMainPermission:" . PermissionEnum::projects->value . ',' . 'view']);
 });
